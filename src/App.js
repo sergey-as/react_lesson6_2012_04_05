77fd869d65c5fa8a810e68ffc5bda92f904ff8dc
sergey-as <sergey_as@ukr.net>
2021-04-22 01:00:43 +0300

    12

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, createContext} from "react";
 
-function App() {
+const CounterContext = createContext();
+console.log(CounterContext);
+
+const ContextProvider = ({children}) => {
+  const [counter, setCounter] = useState(0);
+
+  const incCounter = () => {
+    setCounter(counter + 1);
+  }
+  const decCounter = () => {
+    setCounter(counter - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CounterContext.Provider>
+      {children}
+    </CounterContext.Provider>
+  )
+}
+
+const Counter = () => {
+
+  return (
+    <h3 onClick={() => setCounter(counter + 1)}>Counter: {counter}</h3>
+  )
+}
+
+const Header = () => {
+
+  return (
+    <h1>Header counter:</h1>
+  )
+}
+
+export default function App() {
+  return (
+    <>
+      <Header/>
+      <Counter/>
+    </>
   );
 }
 
-export default App;
+
+// React-context
+// https://www.youtube.com/watch?v=IB140yvLfh8
+// 8:56
